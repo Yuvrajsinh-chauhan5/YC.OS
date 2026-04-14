462d1712c9e2697d3e460f8e81c0b5deb87a5eb1
Yuvrajsinh-chauhan5 <yuvichauhan3112005@gmail.com>
2026-04-14 20:53:32 +0530

    Update about page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -29,9 +29,9 @@ export default function AboutPage() {
   Gandhinagar University<br /><br />
 
   B.Tech in Computer Engineering (2023–2027)<br />
- <br /><br />
+ // <br /><br />
 
-  Backend Systems • Software Architecture • Full-Stack Development • AI/ML Systems • Scalable Engineering
+ //  Backend Systems • Software Architecture • Full-Stack Development • AI/ML Systems • Scalable Engineering
 </p>
 </div>
 
