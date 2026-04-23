72c17c9bdb019e8ba91931babb1dbcf485dbb9e3
Yuvrajsinh-chauhan5 <yuvichauhan3112005@gmail.com>
2026-04-23 19:30:56 +0530

    Update BootGate.jsx

--- src/components/boot/BootGate.jsx
+++ src/components/boot/BootGate.jsx
@@ -2,43 +2,85 @@
 
 
 
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import TerminalBoot from "@/components/boot/TerminalBoot";
+
+// export default function BootGate({ children }) {
+//   const [booted, setBooted] = useState(false);
+//   const [checking, setChecking] = useState(true);
+
+//   useEffect(() => {
+//     const bootTime = localStorage.getItem("bootedAt");
+//     const THIRTY_MIN = 30 * 60 * 1000;
+
+//     if (!bootTime) {
+//       setChecking(false);
+//       return;
+//     }
+
+//     const diff = Date.now() - Number(bootTime);
+
+//     if (diff < THIRTY_MIN) {
+//       setBooted(true);
+//     } else {
+//       localStorage.removeItem("bootedAt");
+//     }
+
+//     setChecking(false);
+//   }, []);
+
+//   const handleBoot = () => {
+//     localStorage.setItem("bootedAt", Date.now());
+//     setBooted(true);
+//   };
+
+//   if (checking) return null;
+
+//   if (!booted) {
+//     return <TerminalBoot onComplete={handleBoot} />;
+//   }
+
+//   return children;
+// }
+
+
+
+
+
+
+
+
 "use client";
 
 import { useEffect, useState } from "react";
-import TerminalBoot from "@/components/boot/TerminalBoot";
+import TerminalBoot from "./TerminalBoot";
 
 export default function BootGate({ children }) {
-  const [booted, setBooted] = useState(false);
-  const [checking, setChecking] = useState(true);
+  const [ready, setReady] = useState(false);
+  const [showBoot, setShowBoot] = useState(false);
 
   useEffect(() => {
-    const bootTime = localStorage.getItem("bootedAt");
-    const THIRTY_MIN = 30 * 60 * 1000;
+    const booted = sessionStorage.getItem("booted");
 
-    if (!bootTime) {
-      setChecking(false);
-      return;
-    }
-
-    const diff = Date.now() - Number(bootTime);
-
-    if (diff < THIRTY_MIN) {
-      setBooted(true);
+    if (!booted) {
+      setShowBoot(true);
     } else {
-      localStorage.removeItem("bootedAt");
+      setReady(true);
     }
-
-    setChecking(false);
   }, []);
 
   const handleBoot = () => {
-    localStorage.setItem("bootedAt", Date.now());
-    setBooted(true);
+    sessionStorage.setItem("booted", "true");
+    setShowBoot(false);
+    setReady(true);
   };
 
-  if (checking) return null;
+  // prevent hydration flash
+  if (!ready && !showBoot) return null;
 
-  if (!booted) {
+  if (showBoot) {
     return <TerminalBoot onComplete={handleBoot} />;
   }
 
